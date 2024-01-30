4bceab33ee04ae68b182f8cacebc3326dfc13240
castdrian <22133246+castdrian@users.noreply.github.com>
2024-01-30 10:13:28 +0100

    fix: catch exceptions in entrypoint

--- index.php
+++ index.php
@@ -5,15 +5,21 @@ require_once ("Employee/employee.php");
 require_once ("Ticket/ticketRepository.php");
 require_once ("Ticket/ticket.php");
 
-$database = new Database();
-$database->connect();
+try {
+    $database = new Database();
+    $database->connect();
 
-// Employee
-$employeeRepository = new EmployeeRepository($database);
-$employee = $employeeRepository->select(1);
-var_dump($employee);
+    // Employee
+    $employeeRepository = new EmployeeRepository($database);
+    $employee = $employeeRepository->select(1);
+    var_dump($employee);
 
-// Ticket
-$ticketRepository = new TicketRepository($database);
-$tickets = $ticketRepository->selectAll();
-var_dump($tickets);
+    // Ticket
+    $ticketRepository = new TicketRepository($database);
+    $tickets = $ticketRepository->selectAll();
+    var_dump($tickets);
+
+} catch (Exception $e) {
+    // Display a user-friendly error message
+    echo "An error occurred: " . $e->getMessage();
+}
