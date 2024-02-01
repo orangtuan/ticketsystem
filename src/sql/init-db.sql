ff77088e801a30b084f6103a0d87e1e888268896
MarioImbroglia <mi@dc.ag>
2024-02-01 08:10:43 +0100

    Extended init db file with more tables

--- src/sql/init-db.sql
+++ src/sql/init-db.sql
@@ -1,28 +1,45 @@
 -- Create 'employee' table
-CREATE TABLE IF NOT EXISTS employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS employee
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE IF NOT EXISTS customer
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    email     VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 -- Create 'ticket_state' table for the TicketState enum
-CREATE TABLE IF NOT EXISTS ticket_state (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ticket_state
+(
+    id    INT AUTO_INCREMENT PRIMARY KEY,
     state VARCHAR(50) NOT NULL UNIQUE
 );
 
 -- Pre-populate 'ticket_state' table with states
-INSERT INTO ticket_state (state) VALUES ('Open'), ('Validated'), ('InProgress'), ('Closed');
+INSERT INTO ticket_state (state)
+VALUES ('Open'),
+       ('Validated'),
+       ('InProgress'),
+       ('Closed');
 
 -- Create 'ticket' table
-CREATE TABLE IF NOT EXISTS ticket (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    creationDate DATETIME NOT NULL,
-    closingDate DATETIME,
-    state_id INT NOT NULL,
-    mail VARCHAR(255),
-    name VARCHAR(255),
-    FOREIGN KEY (state_id) REFERENCES ticket_state(id)
+CREATE TABLE IF NOT EXISTS ticket
+(
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    state_id     INT NOT NULL,
+    customer_id  INT NOT NULL,
+    employee_id  INT NOT NULL,
+    title        VARCHAR(255) NOT NULL,
+    description  TEXT         NOT NULL,
+    creationDate DATETIME     NOT NULL,
+    closingDate  DATETIME     NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES ticket_state (id),
+    FOREIGN KEY (customer_id) REFERENCES customer (id),
+    FOREIGN KEY (employee_id) REFERENCES employee (id)
 );
