fb675739f21b6c33556f6d9e353ddb4b7d89c13a
tz <tuan-anh@gmx.de>
2024-02-08 17:22:02 +0100

    fix and split queries for readability

--- src/sql/init-db.sql
+++ src/sql/init-db.sql
@@ -1,64 +1,108 @@
 -- Create 'employee' table
-CREATE TABLE IF NOT EXISTS employee
-(
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    name     VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-
+CREATE TABLE IF NOT EXISTS `employee` (
+    `id`       INT(11)      NOT NULL,
+    `name`     VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS customer
-(
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    email     VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL
+ALTER TABLE `employee`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `employee`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Create 'customer' table
+CREATE TABLE IF NOT EXISTS `customer` (
+    `id`    INT(11)         NOT NULL,
+    `email` VARCHAR(255)    NOT NULL,
+    `name`  VARCHAR(255)    NOT NULL
 );
+
+ALTER TABLE `customer`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `customer`
+    MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
 
 -- Create 'ticket_state' table for the TicketState enum
-CREATE TABLE IF NOT EXISTS ticket_state
+CREATE TABLE IF NOT EXISTS `ticket_state`
 (
-    id    INT AUTO_INCREMENT PRIMARY KEY,
-    state VARCHAR(50) NOT NULL UNIQUE
+    `id`    INT(11)     NOT NULL,
+    `state` VARCHAR(50) NOT NULL
 );
+
+ALTER TABLE `ticket_state`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `ticket_state`
+    MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `ticket_state`
+    MODIFY `state` VARCHAR(50) NOT NULL UNIQUE;
 
 -- Pre-populate 'ticket_state' table with states
-INSERT INTO ticket_state (state)
-VALUES ('Open'),
-       ('Validated'),
-       ('InProgress'),
-       ('Closed');
+INSERT INTO `ticket_state` (state)
+    VALUES ('Open'),
+           ('Validated'),
+           ('InProgress'),
+           ('Closed');
 
 -- Create 'ticket' table
-CREATE TABLE IF NOT EXISTS ticket
+CREATE TABLE IF NOT EXISTS `ticket`
 (
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    state_id     INT NOT NULL,
-    customer_id  INT NOT NULL,
-    employee_id  INT NOT NULL,
-    title        VARCHAR(255) NOT NULL,
-    description  TEXT         NULL,
-    creationDate DATETIME     NOT NULL,
-    closingDate  DATETIME     NULL,
-    FOREIGN KEY (state_id) REFERENCES ticket_state (id),
-    FOREIGN KEY (customer_id) REFERENCES customer (id),
-    FOREIGN KEY (employee_id) REFERENCES employee (id)
+    `id`           INT(11)      NOT NULL,
+    `state_id`     INT(11)      NOT NULL,
+    `customer_id`  INT(11)      NOT NULL,
+    `employee_id`  INT(11)      NOT NULL,
+    `title`        VARCHAR(255) NOT NULL,
+    `description`  TEXT         NULL,
+    `creationDate` DATETIME     NOT NULL,
+    `closingDate`  DATETIME     NULL
 );
 
+ALTER TABLE `ticket`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `ticket`
+    MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `ticket`
+    ADD FOREIGN KEY (`state_id`)    REFERENCES `ticket_state` (`id`),
+    ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
+    ADD FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+
 -- Create 'message' table
-create table message
-(
-    id          int auto_increment,
-    ticket_id int not null,
-    employee_id int  null,
-    customer_id int  null,
-    message     text not null,
-    constraint id
-        primary key (id),
-    constraint ticket_id
-        foreign key (ticket_id) references ticket (id),
-    constraint customer_id
-        foreign key (customer_id) references customer (id),
-    constraint employee_id
-        foreign key (employee_id) references employee (id)
-)
-    comment 'messages of tickets';
+CREATE TABLE IF NOT EXISTS `message` (
+    `id`          INT   NOT NULL,
+    `ticket_id`   INT   NOT NULL,
+    `employee_id` INT   NULL,
+    `customer_id` INT   NULL,
+    `message`     TEXT  NOT NULL
+);
+
+ALTER TABLE `message`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `message`
+    MODIFY `id` INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `message`
+    ADD CONSTRAINT `ticket_id`
+        FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`id`);
+
+ALTER TABLE `message`
+    ADD CONSTRAINT `customer_id`
+        FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `message`
+    ADD CONSTRAINT `employee_id`
+        FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+
+
+
+-- DO NOT USE IN CASE OF FIRE --
+-- DROP TABLE message;
+-- DROP TABLE ticket;
+-- DROP TABLE ticket_state;
+-- DROP TABLE employee;
+-- DROP TABLE customer;
